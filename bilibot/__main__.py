c318387652b25d4f60efe04fc924df2769e9c496
ww-rm <ww-rm@qq.com>
2021-06-16 20:55:43 +0800

    fix bug

--- bilibot/__main__.py
+++ bilibot/__main__.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("config")
     parser.add_argument("rsakey")
-    parser.add_argument("--test", action="store_ture", default=False)
+    parser.add_argument("--test", action="store_true", default=False)
     args = parser.parse_args()
 
     with open(args.config, "r", encoding="utf8") as f:
--- dailysignin/__main__.py
+++ dailysignin/__main__.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("config")
     parser.add_argument("rsakey")
-    parser.add_argument("--test", action="store_ture", default=False)
+    parser.add_argument("--test", action="store_true", default=False)
 
     args = parser.parse_args()
 
