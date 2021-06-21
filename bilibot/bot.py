0a9f6063843fd4c7622e27c188b613111e0938ff
ww-rm <ww-rm@qq.com>
2021-06-21 14:18:39 +0800

    adjust structure

--- bilibot/bot.py
+++ bilibot/bot.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import List, Union
 
 from utils import xsession
+import cv2
 
 
 class Bot:
@@ -21,6 +22,98 @@ class Bot:
 
         self.s = xsession.Bilibili(self.log_path)
         self.s.headers.update(self.headers)
+
+    def _get_safe_pixiv_illust_ids(self, num: int, history: List[str], blacklist: List[str]) -> list:
+        """
+        Args
+
+        num:
+            how many illust ids should return
+        history: 
+            A list of dynamic illust history, to avoid upload same illusts
+        blacklist: 
+            A list of user ids, to avoid copyright problem
+
+        Returns:
+            A list of illust info, 
+            {
+                "id": str
+                "user_id": str
+                "username": str
+                "url": str
+                "local_path": Path
+            }
+        """
+
+        s_pixiv = xsession.Pixiv(self.log_path)
+        s_pixiv.headers.update(self.headers)
+        # s_pixiv.proxies.update(self.proxies)
+        history = history or []
+        blacklist = blacklist or []
+
+        # get proper illust info
+        dynamic_illust_info = []
+        # {
+        #     "id": "",
+        #     "user_id": "",
+        #     "username": "",
+        #     "url": ""
+        # }
+        cur_date = None
+        while len(dynamic_illust_info) < num:
+            rankings = s_pixiv.get_ranking(date=cur_date, content="illust", mode="monthly")
+            if rankings:
+                cur_date = rankings.get("prev_date")
+                # choose no sexy illusts
+                illust_ids = [
+                    str(e.get("illust_id"))
+                    for e in rankings.get("contents")
+                    if e.get("illust_content_type").get("sexual") == 0  # set rules by ranking info
+                ]
+
+                # choose proper illust
+                for illust_id in illust_ids:
+                    if illust_id not in history:
+                        illust_info = s_pixiv.get_illust(illust_id)
+                        if illust_info:
+                            # set rules by illust info
+                            if (illust_info.get("userId") not in blacklist) and (illust_info.get("pageCount") == 1):
+                                # not in history and user not in blacklist and pagecount == 1
+                                dynamic_illust_info.append(
+                                    {
+                                        "id": illust_id,
+                                        "user_id": illust_info.get("userId"),
+                                        "username": illust_info.get("userName"),
+                                        "url": illust_info.get("urls").get("original")
+                                    }
+                                )
+
+        # cache illust data
+        success_illust_info = []
+        for illust_info in dynamic_illust_info[:num]:
+            url: str = illust_info.get("url")
+            path = Path("tmp", url.split("/")[-1])
+            # download image if not exist
+            if not path.is_file():
+                image_data = s_pixiv.get_page(url)
+                if image_data:
+                    path.write_bytes(image_data)
+                    illust_info["local_path"] = path
+                    success_illust_info.append(illust_info)
+
+        return success_illust_info
+
+    def _get_random_bgm(self, playlist: str) -> dict:
+        """
+        Args
+
+        playlist: 
+            playlist id for kuwo music
+        
+        Returns
+
+            Info of bgm
+        """
 
     def log(self, msg, title="Bilibot"):
         current_time = datetime.utcnow().isoformat(timespec="seconds")
@@ -66,53 +159,8 @@ class Bot:
             "illust_ids": ["xxx", "xxxx"]
         }
         """
-        s_pixiv = xsession.Pixiv(self.log_path)
-        s_pixiv.headers.update(self.headers)
-        # s_pixiv.proxies.update(self.proxies)
-        history = history or []
-        blacklist = blacklist or []
-
-        # get proper illust info
-        dynamic_illust_info = []
-        cur_date = None
-        while len(dynamic_illust_info) < 9:
-            rankings = s_pixiv.get_ranking(date=cur_date, content="illust", mode="monthly")
-            if rankings:
-                cur_date = rankings.get("prev_date")
-                # choose no sexy illusts
-                illust_ids = [
-                    str(e.get("illust_id"))
-                    for e in rankings.get("contents")
-                    if e.get("illust_content_type").get("sexual") == 0
-                ]
-
-                # choose proper illust
-                for illust_id in illust_ids:
-                    if illust_id not in history:
-                        illust_info = s_pixiv.get_illust(illust_id)
-                        if illust_info:
-                            if (illust_info.get("userId") not in blacklist) and (illust_info.get("pageCount") == 1):
-                                # not in history and user not in blacklist and pagecount == 1
-                                dynamic_illust_info.append(
-                                    {
-                                        "id": illust_id,
-                                        "user_id": illust_info.get("userId"),
-                                        "username": illust_info.get("userName"),
-                                        "url": illust_info.get("urls").get("original")
-                                    }
-                                )
-
-        # cache illust data
-        local_illust_paths = []
-        success_illust_info = []
-        for illust_info in dynamic_illust_info[:9]:
-            url: str = illust_info.get("url")
-            image_data = s_pixiv.get_page(url)
-            if image_data:
-                path = Path("tmp", url.split("/")[-1])
-                path.write_bytes(image_data)
-                local_illust_paths.append(path)
-                success_illust_info.append(illust_info)
+        success_illust_info = self._get_safe_pixiv_illust_ids(9, history, blacklist)
+        local_illust_paths = [e.get("local_path") for e in success_illust_info]
 
         # make text contents
         contents = "\u0023\u6bcf\u65e5\u7f8e\u56fe\u0023 \u0023\u0050\u0049\u0058\u0049\u0056\u7f8e\u56fe\u0023 \n"
