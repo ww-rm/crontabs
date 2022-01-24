68a030f14dfc8b69a8d5573f06ffca9d85189a5a
ww-rm <ww-rm@qq.com>
2022-01-24 15:00:36 +0800

    add test

--- scripts/ping_test.sh
+++
@@ -1,3 +0,0 @@
-echo "hello"
-#ping -c 4 www.pixiv.net
-#ping -c 4 api.aliyundrive.com
---
+++ utils/connection_test.py
@@ -0,0 +1,35 @@
+import argparse
+import time
+
+import requests
+
+
+def test_connection(url: str, count: int = 4) -> str:
+    times = []
+    for i in range(count):
+        t_start = time.perf_counter()
+        try:
+            requests.head(url)
+        except:
+            times.append(-1)
+            continue
+        else:
+            t_end = time.perf_counter()
+            times.append(t_end - t_start)
+    
+    result = "Connection test for url: {}\n".format(url)
+    for i, t in enumerate(times):
+        result += "Count:\t{}\tElapsed Time:\t{:.6f} s\t\n".format(i, t)
+    result += "Average Time:\t{:.6f} s\t\n"
+
+    return result
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", dest="url", type=str, required=True, help="Site url to test.")
+    parser.add_argument("-c", dest="count", type=int, required=False, help="Times to test.", default=4)
+
+    args = parser.parse_args()
+
+    test_result = test_connection(args.url, args.count)
+    print(test_result)
