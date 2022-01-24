09a9343e13c56bf89cd087482db5f6a5399d39b7
ww-rm <ww-rm@qq.com>
2022-01-24 15:08:58 +0800

    add test

--- utils/connection_test.py
+++ utils/connection_test.py
@@ -16,13 +16,14 @@ def test_connection(url: str, count: int = 4) -> str:
         else:
             t_end = time.perf_counter()
             times.append(t_end - t_start)
-    
+
     result = "Connection test for url: {}\n".format(url)
     for i, t in enumerate(times):
         result += "Count:\t{}\tElapsed Time:\t{:.6f} s\t\n".format(i, t)
-    result += "Average Time:\t{:.6f} s\t\n"
+    result += "Average Time:\t{:.6f} s\t\n".format(sum(times)/count)
 
     return result
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
