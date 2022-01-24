9ad6d36342f98ea0229d732b8a829014f3718725
ww-rm <ww-rm@qq.com>
2022-01-24 14:36:17 +0800

    test

--- scripts/runtest/script.py
+++ scripts/runtest/script.py
@@ -1,7 +1,19 @@
 import requests
 import os
+import time
 
 if __name__ == "__main__":
     code = os.system("ping -c 4 api.aliyundrive.com")
     print(code)
+
+    times = []
+    for i in range(4):
+        start = time.perf_counter()
+        r = requests.head("https://www.api.aliyundrive.com")
+        end = time.perf_counter()
+        times.append(end-start)
+
+    for i, t in enumerate(times):
+        print(f"Time {i}: {t:.6f} s")
+
     print("Hello world!")
