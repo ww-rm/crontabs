3529361de3ba89f6841435fcf1d54a89012d2773
ww-rm <ww-rm@qq.com>
2022-05-23 16:55:50 +0800

    add sit

--- dailysignin/__main__.py
+++ dailysignin/__main__.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 import utils
 
-from . import (freevpn_cyou, jike0_com, kkwcy_com, socloud_me,
+from . import (freevpn_cyou, jike0_com, kkwcy_com, socloud_me, www_q88q_cyou,
                ssru6_pw, www_bilibili_com, www_hmoe11_net)
 
 
@@ -23,6 +23,7 @@ def run(config: dict):
     jike0_com.Signer(_d(keys["jike0.com"]["usrn"]), _d(keys["jike0.com"]["pwd"])).signin()
     socloud_me.Signer(_d(keys["socloud.me"]["usrn"]), _d(keys["socloud.me"]["pwd"])).signin()
     ssru6_pw.Signer(_d(keys["ssru6.pw"]["usrn"]), _d(keys["ssru6.pw"]["pwd"])).signin()
+    www_q88q_cyou.Signer(_d(keys["www.q88q.cyou"]["usrn"]), _d(keys["www.q88q.cyou"]["pwd"])).signin()
     freevpn_cyou.Signer(_d(keys["freevpn.cyou"]["usrn"]), _d(keys["freevpn.cyou"]["pwd"])).signin()
 
     cookies = dict(map(lambda item: (item[0], _d(item[1])), keys["www.bilibili.com"]["cookies"].items()))
